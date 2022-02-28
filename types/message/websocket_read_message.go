d0b9b59da5ad51e34e9ea8046fc13e36fb0468e7
arma7x <arma7x@live.com>
2022-03-01 05:01:34 +0800

    workaround + types

--- types/message/websocket_read_message.go
+++ types/message/websocket_read_message.go
@@ -6,43 +6,22 @@ import (
 
 type ReadKind int
 
-// TODO
 const (
-	VERIFY_DEVICE 		ReadKind = iota
-	DELETE_MESSAGE 		// id
-	GET_MESSAGE 			// id
-	GET_MESSAGES 			// filter, reverseOrder
-	MARK_MESSAGE_READ // id, isRead
-	SEND_SMS					// []number, message
-	SEND_MMS					// param
-	GET_THREADS
-	RETRIEVE_MMS 			// id
-	// CONTACT
-	// CONTACT_SYNC // bulk
-	// CALENDAR
-	// CALENDAR_SYNC // bulk
-	// CALL ??
+	ACK				ReadKind = iota
+	MESSAGE
+	MESSAGES
+	THREADS
+	MMS
 )
 
 type publicReadMessage struct {
-	Action	string		`json:"action"`
 	Kind 		ReadKind	`json:"kind"`
 	Content	string		`json:"content"`
 }
 
 type ReadMessage struct {
-	action 	string
 	kind		ReadKind
 	content string
-}
-
-func (r *ReadMessage) GetAction() string {
-	return r.action
-}
-
-func (r *ReadMessage) setAction(action string) string {
-	r.action = action
-	return r.action
 }
 
 func (r *ReadMessage) GetKind() ReadKind {
@@ -64,7 +43,7 @@ func (r *ReadMessage) setContent(content string) string {
 }
 
 func (r *ReadMessage) MarshalJSON() ([]byte, error) {
-	j, err := json.Marshal(publicReadMessage{Action: r.action, Kind: r.kind, Content: r.content})
+	j, err := json.Marshal(publicReadMessage{Kind: r.kind, Content: r.content})
 	if err != nil {
 		return nil, err
 	}
@@ -76,7 +55,6 @@ func (r *ReadMessage) UnmarshalJSON(data []byte) (*ReadMessage, error) {
 	if err := json.Unmarshal(data, &public); err != nil {
 		return r, err
 	}
-	r.setAction(public.Action);
 	r.setKind(public.Kind);
 	r.setContent(public.Content);
 	return r, nil
---
+++ types/message/websocket_write_message.go
@@ -0,0 +1,64 @@
+package message
+
+import (
+	"encoding/json"
+)
+
+type WriteKind int
+
+const (
+	DELETE_MESSAGE		WriteKind = iota // content: id
+	GET_MESSAGE 			// content: id
+	GET_MESSAGES 			// content: filter, reverseOrder
+	MARK_MESSAGE_READ // content: id, isRead
+	SEND_SMS					// content: []number, message
+	SEND_MMS					// content: param
+	GET_THREADS
+	RETRIEVE_MMS 			// content: id
+)
+
+type publicWriteMessage struct {
+	Kind 		WriteKind	`json:"kind"`
+	Content	string		`json:"content"`
+}
+
+type WriteMessage struct {
+	kind		WriteKind
+	content string
+}
+
+func (r *WriteMessage) GetKind() WriteKind {
+	return r.kind
+}
+
+func (r *WriteMessage) setKind(kind WriteKind) WriteKind {
+	r.kind = kind
+	return r.kind
+}
+
+func (r *WriteMessage) GetContent() string {
+	return r.content
+}
+
+func (r *WriteMessage) setContent(content string) string {
+	r.content = content
+	return r.content
+}
+
+func (r *WriteMessage) MarshalJSON() ([]byte, error) {
+	j, err := json.Marshal(publicWriteMessage{ Kind: r.kind, Content: r.content})
+	if err != nil {
+		return nil, err
+	}
+	return j, nil
+}
+
+func (r *WriteMessage) UnmarshalJSON(data []byte) (*WriteMessage, error) {
+	var public *publicWriteMessage
+	if err := json.Unmarshal(data, &public); err != nil {
+		return r, err
+	}
+	r.setKind(public.Kind);
+	r.setContent(public.Content);
+	return r, nil
+}
