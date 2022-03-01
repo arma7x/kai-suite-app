15e52df832dd43e67c09df0e644649eef13a18a8
arma7x <arma7x@live.com>
2022-03-02 00:46:15 +0800

    add basic menu + layout

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net"
 	"strconv"
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
@@ -13,10 +14,11 @@ import (
 )
 
 var port string = "4444"
+var content *fyne.Container
 var buttonText = make(chan string)
-var statusLabel = widget.NewLabel("KaiOS PC Suite")
+var statusLabel = widget.NewLabel("Disconnected")
 var ipPortLabel = widget.NewLabel("Ip Address: " + getLocalIP() + ":" + port)
-var button = widget.NewButton("Connect", func() {
+var buttonConnect = widget.NewButton("Connect", func() {
 	if websocketserver.Status == false {
 		addr, err := configuration.CheckIPAddress(getLocalIP(), port);
 		if err != nil {
@@ -75,22 +77,94 @@ func saveIpPort(addr, port string) error {
 	return nil
 }
 
+func renderConnectContent() {
+	for _, l := range content.Objects {
+		content.Remove(l);
+	}
+	content.Add(
+		container.NewVBox(
+			statusLabel,
+			ipPortLabel,
+			buttonConnect,
+		),
+	)
+}
+
+func renderMessagesContent() {
+	for _, l := range content.Objects {
+		content.Remove(l);
+	}
+	content.Add(
+		container.NewVBox(
+			widget.NewLabel("Messages"),
+		),
+	)
+}
+
+func renderContactsContent() {
+	for _, l := range content.Objects {
+		content.Remove(l);
+	}
+	content.Add(
+		container.NewVBox(
+			widget.NewLabel("Contacts"),
+		),
+	)
+}
+
+func renderGAContent() {
+	for _, l := range content.Objects {
+		content.Remove(l);
+	}
+	content.Add(
+		container.NewVBox(
+			widget.NewLabel("Google Account"),
+		),
+	)
+}
+
 func main() {
 	go func() {
 		for {
 			select {
 				case txt := <- buttonText:
-					button.SetText(txt)
+					buttonConnect.SetText(txt)
 			}
 		}
 	}()
 	log.Info("main", configuration.RootPath)
 	app := app.New()
 	window := app.NewWindow("Kai Suite")
+	var menu *fyne.Container = container.NewVBox(
+		widget.NewButton("Connection", func() {
+			renderConnectContent()
+			content.Refresh()
+		}),
+		widget.NewButton("Messages", func() {
+			renderMessagesContent()
+			content.Refresh()
+		}),
+		widget.NewButton("Contacts", func() {
+			renderContactsContent()
+			content.Refresh()
+		}),
+		widget.NewButton("Google Account", func() {
+			renderGAContent()
+			content.Refresh()
+		}),
+	)
+	size := menu.Size()
+	size.Width = 20
+	menu.Resize(size)
+	content = container.NewMax()
+	renderConnectContent()
 	window.SetContent(container.NewVBox(
-		statusLabel,
-		ipPortLabel,
-		button,
+		container.NewHBox(widget.NewLabel("KaiOS PC Suite")),
+		container.NewHBox(
+			menu,
+			content,
+		),
 	))
 	window.ShowAndRun()
 }
+
