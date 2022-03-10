e48a962b843f7387e62cf92610db040683821e86
arma7x <arma7x@live.com>
2022-03-11 05:14:27 +0800

    WIP sync contacts protocol

--- types/misc.go
+++ types/misc.go
@@ -3,6 +3,7 @@ package types
 import(
 	"golang.org/x/oauth2"
 	google_oauth2 "google.golang.org/api/oauth2/v2"
+	"google.golang.org/api/people/v1"
 )
 
 type UserInfoAndToken struct {
@@ -11,8 +12,34 @@ type UserInfoAndToken struct {
 }
 
 type Metadata struct {
-	SyncID 				string	`json:"sync_id,omitempty"`
-	SyncUpdated		string	`json:"sync_updated,omitempty"`
+	SyncID 				string	`json:"sync_id,omitempty"`			//KaiContact.id
+	SyncUpdated		string	`json:"sync_updated,omitempty"`	//KaiContact.updated
 	Hash					string	`json:"hash,omitempty"`
 	Deleted 			bool		`json:"deleted,omitempty"`
+}
+
+type KaiContact struct {}
+
+type TxSync struct {
+	Namespace string				`json:"namespace"`	//account:people:id
+	Metadata								`json:"metadata"`
+	Person people.Person		`json:"person"`
+}
+
+// successfully add or update contact data on kaios
+type RxSyncFlag2 struct {
+	Namespace			string	`json:"namespace"`	//account:people:id
+	SyncID				string	`json:"sync_id"`
+	SyncUpdated		string	`json:"sync_updated"`
+}
+
+// received kaicontact from kaios then TxSync for comformation
+type RxSyncFlag4 struct {
+	Namespace			string	`json:"namespace"`	//local:people:KaiContact.id
+	KaiContact						`json:"kai_contact"`
+}
+
+// both desktop & kaios remove this contact
+type RxSyncFlag6 struct {
+	Namespace			string	`json:"namespace"`	//account:people:id
 }
