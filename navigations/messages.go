601c50c8fd19f48d0405f6999e944d8a1ad35424
arma7x <arma7x@live.com>
2022-03-17 15:45:13 +0800

    Mock messages UI

--- navigations/messages.go
+++ navigations/messages.go
@@ -1,79 +1,124 @@
 package navigations
 
 import (
+	"strconv"
+	"kai-suite/types"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 	log "github.com/sirupsen/logrus"
-	"google.golang.org/api/people/v1"
+	custom_widget "kai-suite/widgets"
 )
 
 type ThreadCardCached struct {
-	Hash string
+	Timestamp int
 	Card fyne.CanvasObject
 }
 
 var (
-	threadsCache map[string]string
+	threadsCardCache map[int]*ThreadCardCached
 	threadsBox *fyne.Container
 	threadsContainer *fyne.Container
 	messagesBox *fyne.Container
 	messagesContainer *fyne.Container
 )
 
-func ViewMessagesThread(namespace string, personsArr []*people.Person) {
+func threadsRepository() []types.MozMobileMessageThread {
+	t1 := types.MozMobileMessageThread{
+		Id: 3,
+		Body: "Help",
+		UnreadCount: 0,
+		Participants: []string{"Hotlink@"}, 
+		Timestamp: 1647463907980,
+		LastMessageSubject: "",
+		LastMessageType: "sms",
+	}
+	t2 := types.MozMobileMessageThread{
+		Id: 2,
+		Body: "To check balance, reply CHECK. To e…",
+		UnreadCount: 0,
+		Participants: []string{"20505"}, 
+		Timestamp: 1647463907980,
+		LastMessageSubject: "",
+		LastMessageType: "1647463583560",
+	}
+	return []types.MozMobileMessageThread{t1, t2}
+}
 
+func ViewMessagesThread(threadId int, threads []types.MozMobileMessageThread) {
+	
+}
+
+func viewThread(threadId int) {
+	threadsBox.Hide()
+	messagesBox.Show()
+	messagesContainer.Objects = nil
+	messagesContainer.Add(widget.NewLabel("T3 Message 1"))
+	messagesContainer.Add(widget.NewLabel("T3 Message 2"))
+	messagesContainer.Add(widget.NewLabel("T3 Message 3"))
+	messagesContainer.Add(widget.NewLabel("T3 Message 4"))
+	messagesContainer.Add(widget.NewLabel("T3 Message 5"))
+	messagesContainer.Add(widget.NewLabel("T3 Message 6"))
 }
 
 func RenderMessagesContent(c *fyne.Container) {
 	log.Info("Messages Rendered")
 	c.Hide()
-	threadsCache = make(map[string]string)
+	threadsCardCache = make(map[int]*ThreadCardCached)
 	threadsContainer = container.NewVBox()
-	threadsContainer.Add(widget.NewButton("Thread1", func() {
-		threadsBox.Hide()
-		messagesBox.Show()
-		messagesContainer.Objects = nil
-		messagesContainer.Add(widget.NewLabel("T1 Message 1"))
-		messagesContainer.Add(widget.NewLabel("T1 Message 2"))
-		messagesContainer.Add(widget.NewLabel("T1 Message 3"))
-		messagesContainer.Add(widget.NewLabel("T1 Message 4"))
-		messagesContainer.Add(widget.NewLabel("T1 Message 5"))
-		messagesContainer.Add(widget.NewLabel("T1 Message 6"))
-	}))
-	threadsContainer.Add(widget.NewButton("Thread2", func() {
-		threadsBox.Hide()
-		messagesBox.Show()
-		messagesContainer.Objects = nil
-		messagesContainer.Add(widget.NewLabel("T2 Message 1"))
-		messagesContainer.Add(widget.NewLabel("T2 Message 2"))
-		messagesContainer.Add(widget.NewLabel("T2 Message 3"))
-		messagesContainer.Add(widget.NewLabel("T2 Message 4"))
-		messagesContainer.Add(widget.NewLabel("T2 Message 5"))
-		messagesContainer.Add(widget.NewLabel("T2 Message 6"))
-	}))
-	threadsContainer.Add(widget.NewButton("Thread3", func() {
-		threadsBox.Hide()
-		messagesBox.Show()
-		messagesContainer.Objects = nil
-		messagesContainer.Add(widget.NewLabel("T3 Message 1"))
-		messagesContainer.Add(widget.NewLabel("T3 Message 2"))
-		messagesContainer.Add(widget.NewLabel("T3 Message 3"))
-		messagesContainer.Add(widget.NewLabel("T3 Message 4"))
-		messagesContainer.Add(widget.NewLabel("T3 Message 5"))
-		messagesContainer.Add(widget.NewLabel("T3 Message 6"))
-	}))
+	for _, t := range threadsRepository() {
+		if _, exist := threadsCardCache[t.Id]; exist == true {
+			if threadsCardCache[t.Id].Timestamp != t.Timestamp {
+				threadsCardCache[t.Id].Timestamp = t.Timestamp
+				threadsCardCache[t.Id] = &ThreadCardCached{}
+				threadsCardCache[t.Id].Timestamp = t.Timestamp
+				card := &widget.Card{}
+				card.SetTitle(t.Body)
+				card.SetSubTitle(t.Participants[0])
+				card.SetContent(container.NewHBox(
+					custom_widget.NewButton(strconv.Itoa(t.Id), "View", func(scope string) {
+					log.Info("Clicked view ", scope)
+					if i, err := strconv.Atoi(scope); err == nil {
+						viewThread(i)
+					}
+				}),
+				))
+				threadsCardCache[t.Id].Card = card
+			}
+		} else {
+			threadsCardCache[t.Id] = &ThreadCardCached{}
+			threadsCardCache[t.Id].Timestamp = t.Timestamp
+			card := &widget.Card{}
+			card.SetTitle(t.Body)
+			card.SetSubTitle(t.Participants[0])
+			card.SetContent(container.NewHBox(
+				custom_widget.NewButton(strconv.Itoa(t.Id), "View", func(scope string) {
+					log.Info("Clicked view ", scope)
+					if i, err := strconv.Atoi(scope); err == nil {
+						viewThread(i)
+					}
+				}),
+			))
+			threadsCardCache[t.Id].Card = card
+		}
+		threadsContainer.Add(threadsCardCache[t.Id].Card)
+	}
 	messagesContainer = container.NewVBox()
 	threadsBox = container.NewBorder(
 		nil, nil, nil, nil,
 		container.NewVScroll(container.NewVBox(threadsContainer)),
 	)
 	messagesBox = container.NewBorder(
-		widget.NewButton("Return", func() {
-			threadsBox.Show()
-			messagesBox.Hide()
-		}),
-		nil, nil, nil,
+		nil,
+		container.NewBorder(
+			nil, nil,
+			widget.NewButton("Return", func() {
+				threadsBox.Show()
+				messagesBox.Hide()
+			}),
+			widget.NewButton("SEND", func(){}),
+			widget.NewMultiLineEntry(),
+		), nil, nil,
 		container.NewVScroll(container.NewVBox(messagesContainer)),
 	)
 	messagesBox.Hide()
--- types/moz_mobile_message_thread.go
+++ types/moz_mobile_message_thread.go
@@ -1,106 +1,15 @@
 package types
 
-import (
-	"encoding/json"
-)
-
-type publicMozMobileMessageThread struct {
-	Id							int				`json:"id"`
-	Body						string		`json:"body"`
-	UnreadCount			int				`json:"unreadCount"`
-	Participants		[]string	`json:"participants"`
-	Timestamp				int				`json:"timestamp"`
-	LastMessageType	string		`json:"lastMessageType"`
-}
+//import (
+	//"encoding/json"
+//)
 
 type MozMobileMessageThread struct {
-	id							int
-	body						string
-	unreadCount			int
-	participants		[]string
-	timestamp				int
-	lastMessageType	string
-}
-
-func (m *MozMobileMessageThread) GetId() int {
-	return m.id
-}
-
-func (m *MozMobileMessageThread) setId(id int) int {
-	m.id = id
-	return m.id
-}
-
-func (m *MozMobileMessageThread) GetBody() string {
-	return m.body
-}
-
-func (m *MozMobileMessageThread) setBody(body string) string {
-	m.body = body
-	return m.body
-}
-
-func (m *MozMobileMessageThread) GetUnreadCount() int {
-	return m.unreadCount
-}
-
-func (m *MozMobileMessageThread) setUnreadCount(unreadCount int) int {
-	m.unreadCount = unreadCount
-	return m.unreadCount
-}
-
-func (m *MozMobileMessageThread) GetParticipants() []string {
-	return m.participants
-}
-
-func (m *MozMobileMessageThread) setParticipants(participants []string) []string {
-	m.participants = participants
-	return m.participants
-}
-
-func (m *MozMobileMessageThread) GetTimestamp() int {
-	return m.timestamp
-}
-
-func (m *MozMobileMessageThread) setTimestamp(timestamp int) int {
-	m.timestamp = timestamp
-	return m.timestamp
-}
-
-func (m *MozMobileMessageThread) GetLastMessageType() string {
-	return m.lastMessageType
-}
-
-func (m *MozMobileMessageThread) setLastMessageType(lastMessageType string) string {
-	m.lastMessageType = lastMessageType
-	return m.lastMessageType
-}
-
-func (m *MozMobileMessageThread) MarshalJSON() ([]byte, error) {
-	j, err := json.Marshal(publicMozMobileMessageThread{
-		Id: m.id,
-		Body: m.body,
-		UnreadCount: m.unreadCount,
-		Participants: m.participants,
-		Timestamp: m.timestamp,
-		LastMessageType: m.lastMessageType,
-	})
-	if err != nil {
-		return nil, err
-	}
-	return j, nil
-}
-
-func (m *MozMobileMessageThread) UnmarshalJSON(data []byte) (*MozMobileMessageThread, error) {
-	var public *publicMozMobileMessageThread
-	if err := json.Unmarshal(data, &public); err != nil {
-		return m, err
-	}
-	m.setId(public.Id);
-	m.setBody(public.Body);
-	m.setUnreadCount(public.UnreadCount);
-	m.setParticipants(public.Participants);
-	m.setTimestamp(public.Timestamp);
-	m.setLastMessageType(public.LastMessageType);
-	return m, nil
+	Id									int				`json:"id"`
+	Body								string		`json:"body"`
+	UnreadCount					int				`json:"unreadCount"`
+	Participants				[]string	`json:"participants"`
+	Timestamp						int				`json:"timestamp"`
+	LastMessageSubject	string		`json:"lastMessageSubject"`
+	LastMessageType			string		`json:"lastMessageType"`
 }
