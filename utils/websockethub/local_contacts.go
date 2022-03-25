91b6bae2aeeab67334ad72476b86e69019c79635
arma7x <arma7x@live.com>
2022-03-25 23:55:08 +0800

    Use http endpoint local-contact to sync/restore local contacts

--- utils/websockethub/local_contacts.go
+++ utils/websockethub/local_contacts.go
@@ -49,33 +49,7 @@ func SyncLocalContacts() {
 		global.CONTACTS_DB.View(func(tx *buntdb.Tx) error {
 			persons := make(map[string]people.Person)
 			metadata := make(map[string]types.Metadata)
-			tx.Ascend("people_local", func(key, val string) bool {
-				//log.Info("person: ", key, ", ", val)
-				var person people.Person
-				if err := json.Unmarshal([]byte(val), &person); err != nil {
-					return true
-				}
-				split := strings.Split(key, ":")
-				//log.Info("Person Key:", split[len(split) - 1])
-				persons[split[len(split) - 1]] = person //TODO
-				return true
-			})
-			tx.Ascend("metadata_local", func(key, val string) bool {
-				//log.Info("metadata_local: ", key, ", ", val)
-				var mt types.Metadata
-				if err := json.Unmarshal([]byte(val), &mt); err != nil {
-					return true
-				}
-				//log.Info("Metadata Key:", mt.SyncID)
-				metadata[mt.SyncID] = mt
-				return true
-			})
-			// log.Info(len(persons), " ", len(metadata))
-			// syncCb(persons, metadata)
-			item := types.TxSyncLocalContact5{
-				Metadata:	metadata,
-				Persons:  persons,
-			}
+			item := types.TxSyncLocalContact5{Metadata:	metadata, Persons: persons}
 			bd, _ := json.Marshal(item)
 			btx, _ := json.Marshal(types.WebsocketMessageFlag {Flag: 5, Data: string(bd)})
 			if err := Client.GetConn().WriteMessage(websocket.TextMessage, btx); err != nil {
@@ -91,33 +65,7 @@ func RestoreLocalContacts() {
 		global.CONTACTS_DB.View(func(tx *buntdb.Tx) error {
 			persons := make(map[string]people.Person)
 			metadata := make(map[string]types.Metadata)
-			tx.Ascend("people_local", func(key, val string) bool {
-				//log.Info("person: ", key, ", ", val)
-				var person people.Person
-				if err := json.Unmarshal([]byte(val), &person); err != nil {
-					return true
-				}
-				split := strings.Split(key, ":")
-				//log.Info("Person Key:", split[len(split) - 1])
-				persons[split[len(split) - 1]] = person //TODO
-				return true
-			})
-			tx.Ascend("metadata_local", func(key, val string) bool {
-				//log.Info("metadata_local: ", key, ", ", val)
-				var mt types.Metadata
-				if err := json.Unmarshal([]byte(val), &mt); err != nil {
-					return true
-				}
-				//log.Info("Metadata Key:", mt.SyncID)
-				metadata[mt.SyncID] = mt
-				return true
-			})
-			// log.Info(len(persons), " ", len(metadata))
-			// syncCb(persons, metadata)
-			item := types.TxRestoreLocalContact7{
-				Metadata:	metadata,
-				Persons:  persons,
-			}
+			item := types.TxRestoreLocalContact7{Metadata: metadata, Persons: persons}
 			bd, _ := json.Marshal(item)
 			btx, _ := json.Marshal(types.WebsocketMessageFlag {Flag: 7, Data: string(bd)})
 			if err := Client.GetConn().WriteMessage(websocket.TextMessage, btx); err != nil {
